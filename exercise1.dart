41766924f15b5cef37fedf51a5b1ce0ea49eb042
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-07 22:34:37 +0300

    finsihed exercise1

--- exercise1.dart
+++ exercise1.dart
@@ -1,14 +1,17 @@
-class Student{
-    String name;
-    int age;
+class Student {
+  String name;
+  int age;
 
-    Student(this.name, this.age);
+  Student(this.name, this.age);
 
-    Student.guest()
-        : name = "Guest", age = 0;
+  Student.guest() : name = "Guest", age = 0;
 }
 
-void main(){
-    Student s = new Student();
-    
+void main() {
+  Student rs = Student("Abel", 20);
+  print(rs.name);
+  print(rs.age);
+  Student s = Student.guest();
+  print(s.name);
+  print(s.age);
 }
